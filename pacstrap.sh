24708a2e3d2d84d03729ef2dff9eb51c6e0e867c
Arthur <nicolaenko99@gmail.com>
2022-06-14 16:53:55 +0300

    Pacstrap script added

---
+++ pacstrap.sh
@@ -0,0 +1,4 @@
+pacstrap /mnt linux linux-firmware base base-dev networkmanager pipewire pipewire-pulse pipewire-alsa pipewire-jack wireplumber \
+phonon-qt5-vlc plasma dolphin ark kitty okular spectacle audacious gwenview htop vim neovim bluedevil keepassxc \
+kate xf86-video-intel intel-ucode lvm2 kvantum zsh zsh-completions git
+`
